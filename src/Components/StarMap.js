f76c390cfd7e36704597a0134f91e3ada38fd73a
Ash Dan <Arvolley01@gmail.com>
2022-08-01 16:20:02 -0400

    fixed links bug

--- src/Components/StarMap.js
+++ src/Components/StarMap.js
@@ -1,12 +1,8 @@
-import { useEffect, useState } from "react"
 
-const StarMap = () => {
 
-  const [source, setSource] = useState("");
+const StarMap = ({setPlanetId}) => {
 
-  useEffect(() => {
-    setSource("https://app.pixelencounter.com/api/basic/planets?frame=13&id=1")
-  }, [])
+  const getUrl = (id) => `https://app.pixelencounter.com/api/basic/planets?frame=13&width=800&height=800&size=500&disableStars=true&disableBackground=true&id=${id}`
 
 
 
@@ -14,7 +10,11 @@ const StarMap = () => {
   return (
     <div>
       <p> Star Map </p>
-      {source && <img src={source} alt="planet" />}
+      <img key="1" src={getUrl(1)} alt="planet 1" onClick={() => {setPlanetId(1)}} />
+      <img key="2" src={getUrl(2)} alt="planet 2" onClick={() => { setPlanetId(2) }} />
+      <img key="3" src={getUrl(3)} alt="planet 3" onClick={() => { setPlanetId(3) }} />
+      <img key="4" src={getUrl(4)} alt="planet 4" onClick={() => { setPlanetId(4) }} />
+      <img key="5" src={getUrl(5)} alt="planet 5" onClick={() => { setPlanetId(5) }} />
     </div>
   )
 }
